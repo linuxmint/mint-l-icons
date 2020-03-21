d99b8eff9decd79649525a12ba3ab54e2fc323ba
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2020-03-21 14:09:05 +0000

    src/places: Add a script to generate color variations

---
+++ src/places/generate-color-variations.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import os
+
+# This script uses green.svg and generates the following files from it:
+#
+# aqua.svg
+# blue.svg
+# brown.svg
+# grey.svg
+# orange.svg
+# pink.svg
+# purple.svg
+# red.svg
+# sand.svg
+# teal.svg
+
+# It uses the following color table to do so:
+COLORS = {}
+COLORS["aqua"] = "66a8cb"
+COLORS["blue"] = "5972c3"
+COLORS["brown"] = "997052"
+COLORS["grey"] = "999999"
+COLORS["orange"] = "cc823f"
+COLORS["pink"] = "ce6ca2"
+COLORS["purple"] = "8463c5"
+COLORS["red"] = "b74c4a"
+COLORS["sand"] = "c4a660"
+COLORS["teal"] = "59c3ad"
+
+GREEN_COLOR = "8bb158"
+
+for color in COLORS:
+    value = COLORS[color]
+    os.system("sed 's/%s/%s/g' green.svg > %s.svg" % (GREEN_COLOR, value, color))
