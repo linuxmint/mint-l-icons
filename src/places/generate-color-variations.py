a9bbd3426dca4be6bd4b368705179db00436ed0e
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2022-11-28 12:25:32 +0000

    Adjust folder color tones

--- src/places/generate-color-variations.py
+++ src/places/generate-color-variations.py
@@ -18,17 +18,17 @@ import os
 # It uses the following color table to do so:
 
 SRC = {"folder":"eeca8f","backfolder":"c89e6b","paper":"e4e4e4","line":"92b372","emblem":"575757"}
-GREEN = {"name":"green","folder":"f9ca70","backfolder":"e19d00","paper":"e4e4e4","line":"35a854","emblem":"1f1f1f"}
-AQUA = {"name":"aqua","folder":"f9ca70","backfolder":"e19d00","paper":"e4e4e4","line":"1f9ede","emblem":"1f1f1f"}
-BLUE = {"name":"blue","folder":"f9ca70","backfolder":"e19d00","paper":"e4e4e4","line":"0c75de","emblem":"1f1f1f"}
-BROWN = {"name":"brown","folder":"f9ca70","backfolder":"e19d00","paper":"e4e4e4","line":"aa876a","emblem":"1f1f1f"}
-GREY = {"name":"grey","folder":"f9ca70","backfolder":"e19d00","paper":"e4e4e4","line":"70737a","emblem":"1f1f1f"}
-ORANGE = {"name":"orange","folder":"f9ca70","backfolder":"e19d00","paper":"e4e4e4","line":"ff7139","emblem":"1f1f1f"}
-PINK = {"name":"pink","folder":"f9ca70","backfolder":"e19d00","paper":"e4e4e4","line":"e54980","emblem":"1f1f1f"}
-PURPLE = {"name":"purple","folder":"f9ca70","backfolder":"e19d00","paper":"e4e4e4","line":"8c5dd9","emblem":"1f1f1f"}
-RED = {"name":"red","folder":"f9ca70","backfolder":"e19d00","paper":"e4e4e4","line":"e82127","emblem":"1f1f1f"}
-SAND = {"name":"sand","folder":"f9ca70","backfolder":"e19d00","paper":"e4e4e4","line":"c5a07c","emblem":"1f1f1f"}
-TEAL = {"name":"teal","folder":"f9ca70","backfolder":"e19d00","paper":"e4e4e4","line":"199ca8","emblem":"1f1f1f"}
+GREEN = {"name":"green","folder":"f9c470","backfolder":"e0a84f","paper":"e4e4e4","line":"35a854","emblem":"4a4a4a"}
+AQUA = {"name":"aqua","folder":"f9c470","backfolder":"e0a84f","paper":"e4e4e4","line":"1f9ede","emblem":"4a4a4a"}
+BLUE = {"name":"blue","folder":"f9c470","backfolder":"e0a84f","paper":"e4e4e4","line":"1f9ede","emblem":"4a4a4a"}
+BROWN = {"name":"brown","folder":"f9c470","backfolder":"e0a84f","paper":"e4e4e4","line":"aa876a","emblem":"4a4a4a"}
+GREY = {"name":"grey","folder":"f9c470","backfolder":"e0a84f","paper":"e4e4e4","line":"70737a","emblem":"4a4a4a"}
+ORANGE = {"name":"orange","folder":"f9c470","backfolder":"e0a84f","paper":"e4e4e4","line":"ff7139","emblem":"4a4a4a"}
+PINK = {"name":"pink","folder":"f9c470","backfolder":"e0a84f","paper":"e4e4e4","line":"e54980","emblem":"4a4a4a"}
+PURPLE = {"name":"purple","folder":"f9c470","backfolder":"e0a84f","paper":"e4e4e4","line":"8c5dd9","emblem":"4a4a4a"}
+RED = {"name":"red","folder":"f9c470","backfolder":"e0a84f","paper":"e4e4e4","line":"e82127","emblem":"4a4a4a"}
+SAND = {"name":"sand","folder":"f9c470","backfolder":"e0a84f","paper":"e4e4e4","line":"c5a07c","emblem":"4a4a4a"}
+TEAL = {"name":"teal","folder":"f9c470","backfolder":"e0a84f","paper":"e4e4e4","line":"199ca8","emblem":"4a4a4a"}
 
 VARIANTS = [GREEN, AQUA, BLUE, BROWN, GREY, ORANGE, PINK, PURPLE, RED, SAND, TEAL]
 
